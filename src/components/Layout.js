56f1204bdf3add02d21743b343ad15056d9fc713
Zamar Roura <zamar.roura@prensaiberica.es>
2021-03-13 19:03:10 +0100

    analytics

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,9 @@ import Navbar from '../components/Navbar'
 import './all.sass'
 import useSiteMetadata from './SiteMetadata'
 import { withPrefix } from 'gatsby'
-
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-111730482-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata()
   return (
@@ -52,7 +54,7 @@ const TemplateWrapper = ({ children }) => {
         <Navbar/>
         <div className="main-container">{children}</div>
       </div>
-      
+
       {/* <Footer /> */}
     </div>
   )
